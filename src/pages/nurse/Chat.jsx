62f21fdacee7eaff83c3eb50598cabeab46bc85a
KimHong0305 <145215715+KimHong0305@users.noreply.github.com>
2025-05-31 02:40:20 +0700

    update chat nurse v2

--- src/pages/nurse/Chat.jsx
+++ src/pages/nurse/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ChatBoxForEmployee from './ChatBoxForEmployee';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -8,65 +8,97 @@ import {
 } from '../../store/Reducers/chat/chatNurseReducer';
 import { toast } from 'react-toastify';
 import { useWebSocket } from '../../contexts/WebSocketContext';
+import { IoChatbubblesOutline } from "react-icons/io5";
 
 const Chat = () => {
     const dispatch = useDispatch();
     const { rooms, roomsWait } = useSelector((state) => state.chat_nurse);
     const [selectedRoom, setSelectedRoom] = useState(null);
     const { messages } = useWebSocket();
-
-    const pendingMessages = messages.filter((msg) => {
-        return !msg.receiverId && !msg.receiver;
-    });
-
-    const waitingRooms = Object.values(
-        pendingMessages.reduce((acc, msg) => {
-            if (!acc[msg.roomId]) {
-                acc[msg.roomId] = {
-                    roomId: msg.roomId,
-                    senderId: msg.senderId,
-                    senderImage: msg.senderImage,
-                    senderName: msg.senderName,
-                    lastMessage: msg.content || msg.lastMessage,
-                    roomStatus: msg.roomStatus || false,
-                };
-            }
-            return acc;
-        }, {})
-    );
-
-    const combinedRooms = [
-        ...waitingRooms,
-        ...rooms.filter(
-            (apiRoom) => !waitingRooms.some((socketRoom) => socketRoom.roomId === apiRoom.roomId)
-        ),
-        ...roomsWait.filter(
-            (historyRoom) =>
-                !waitingRooms.some((r) => r.roomId === historyRoom.roomId) &&
-                !rooms.some((r) => r.roomId === historyRoom.roomId)
-        ),
-    ];
+    const [unreadCount, setUnreadCount] = useState(0);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef();
+    const [combinedRooms, setCombinedRooms] = useState([]);
 
     useEffect(() => {
-        dispatch(getRoomVacant());
         dispatch(historyChatRoom());
     }, [dispatch]);
 
-    // console.log(combinedRooms)
+    useEffect(() => {
+        if (roomsWait.length > 0 && combinedRooms.length === 0) {
+            setCombinedRooms(roomsWait);
+        }
+    }, [roomsWait, combinedRooms]);
+
+    useEffect(() => {
+        const pendingMessages = messages.filter((msg) => {
+            return !msg.receiverId && !msg.receiver;
+        });
+        setUnreadCount(pendingMessages.length);
+    }, [messages, showDropdown]);
+
+    useEffect(() => {
+        const handledRoomIds = combinedRooms.map((room) => room.roomId);
+
+        const pendingMessages = messages.filter((msg) => {
+            return (
+                !msg.receiverId &&
+                !msg.receiver &&
+                !handledRoomIds.includes(msg.roomId)
+            );
+        });
+        setUnreadCount(pendingMessages.length);
+    }, [messages, showDropdown, combinedRooms]);
+
+    const handleToggleDropdown = () => {
+        setShowDropdown((prev) => {
+            const newState = !prev;
+
+            if (!prev) {
+                dispatch(getRoomVacant());
+            }
+
+            return newState;
+        });
+    };
 
     const handleChooseRoom = async (room) => {
         if (room.roomStatus === true) {
             try {
-                await dispatch(chooseRoomVacant(room.roomId)).unwrap();
-                setSelectedRoom({ roomId: room.roomId, receiver: room.senderId, image: room.senderImage });
-                toast.success('Hãy nói chuyện nhẹ nhàng với khách hàng nhé!');
+                const result = await dispatch(chooseRoomVacant(room.roomId)).unwrap();
+                toast.success(result.message);
+
+                setCombinedRooms(prev => {
+                    const exists = prev.some(r => r.roomId === room.roomId);
+                    return exists ? prev : [room, ...prev];
+                });
+
+                setShowDropdown(false);
             } catch (err) {
                 toast.error(err);
+                return;
             }
+        } else {
+            setCombinedRooms(prev => {
+                const exists = prev.some(r => r.roomId === room.roomId);
+                return exists ? prev : [room, ...prev];
+            });
         }
-        else {
-            setSelectedRoom({ roomId: room.roomId, receiver: room.senderId, image: room.senderImage });
-        }
+
+        setSelectedRoom({
+            roomId: room.roomId,
+            receiver: room.senderId,
+            image: room.senderImage,
+        });
+    };
+
+
+    const handleChoose = async (room) => {
+        setSelectedRoom({
+            roomId: room.roomId,
+            receiver: room.senderId,
+            image: room.senderImage,
+        });
     };
 
     const formatLastTimeShort = (lastTime) => {
@@ -91,8 +123,49 @@ const Chat = () => {
 
     return (
         <div className="flex min-h-[500px] p-4 gap-6 bg-gray-100">
-            <div className="w-1/3 bg-white rounded-lg shadow-lg overflow-y-auto max-h-[550px]">
-                <h2 className="text-xl font-semibold sticky top-0 bg-white z-10 p-4 ">Đoạn chat</h2>
+            <div className="w-1/3 bg-white rounded-lg shadow-lg py-4 overflow-y-auto relative max-h-[550px]" ref={dropdownRef}>
+                <div className='flex justify-between items-center mb-4'>
+                    <h2 className="text-xl px-4 font-semibold">Đoạn chat</h2>
+                    <div className="relative cursor-pointer mr-2" onClick={handleToggleDropdown}>
+                        <IoChatbubblesOutline className='size-6' />
+                        {unreadCount > 0 && (
+                            <span 
+                                className="absolute -top-1 -right-1 bg-red-600 text-white text-xs font-bold rounded-full w-4 h-4 flex items-center justify-center"
+                            >
+                                {unreadCount}
+                            </span>
+                        )}
+                    </div>
+                </div>
+
+                {showDropdown && (
+                    <div className="absolute right-0 top-12 w-72 max-h-96 overflow-auto bg-white border rounded shadow-lg z-20">
+                        {rooms.length === 0 ? (
+                            <p className="p-4 text-gray-500">Không có khách hàng chờ</p>
+                        ) : (
+                            rooms.map((room) => (
+                                <div
+                                    key={room.roomId}
+                                    className="cursor-pointer py-3 px-4 border-b hover:bg-gray-100 flex items-center"
+                                    onClick={() => handleChooseRoom(room)}
+                                >
+                                    <img
+                                        src={room.senderImage}
+                                        alt={room.senderName}
+                                        className="w-10 h-10 rounded-full object-cover border-2 mr-3"
+                                    />
+                                    <div>
+                                        <p className="text-base font-medium">{room.senderName || 'Khách không tên'}</p>
+                                        <p className="text-sm text-gray-500">{room.lastMessage} 
+                                            <span className='font-semibold'>.{formatLastTimeShort(room.lastTime)}</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            ))
+                        )}
+                    </div>
+                )}
+                
                 {combinedRooms.length === 0 ? (
                     <p className="text-gray-500 px-5"></p>
                 ) : (
@@ -102,32 +175,27 @@ const Chat = () => {
                             className={`cursor-pointer py-3 px-2 border-b rounded relative
                                 ${selectedRoom?.roomId === room.roomId ? 'bg-blue-100' : 'hover:bg-gray-100'}
                             `}
-                            onClick={() => handleChooseRoom(room)}
+                            onClick={() => handleChoose(room)}
                         >
 
                             <div className="flex justify-start items-center">
                                 <img
                                     src={room.senderImage}
                                     alt={room.senderName}
-                                    className="w-16 h-16 rounded-full object-cover border-2 mr-2"
+                                    className="w-16 h-16 rounded-full object-cover border-2"
                                 />
                                 <div>
                                     <p className="font-medium">{room.senderName || 'Khách không tên'}</p>
-                                    <div className="flex items-center justify-between w-[310px]">
+                                    <div className="flex items-center justify-between w-[300px]">
                                         <p className="text-sm text-gray-500 truncate w-[250px]">
                                             {room.lastMessage}
                                         </p>
-                                        <p className="text-sm text-gray-400 mr-2">
+                                        <p className="text-sm text-gray-400">
                                             {formatLastTimeShort(room.lastTime)}
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            {room.roomStatus === true && (
-                                <span className="absolute top-3 right-3 bg-red-500 text-white text-xs font-bold px-2 py-0.5 rounded-full">
-                                    New
-                                </span>
-                            )}
                         </div>
                     ))
                 )}
